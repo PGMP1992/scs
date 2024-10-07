f5f7cf59b0dace3e8872136fda6c26a2a2bacda8
Pedro <pedro@dkltd.net>
2024-10-07 16:40:24 +0200

    Booking.Index  Layout Done.

--- SCSWeb/Areas/Customer/BookingController.cs
+++ SCSWeb/Areas/Customer/BookingController.cs
@@ -27,22 +27,23 @@ namespace SCSWeb.Areas.Customer
 
         public async Task<IActionResult> Index()
         {
-            var userId = HttpContext.User.GetUserId();
+            //var userId = HttpContext.User.GetUserId();
 
-            IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader
-                .GetAll(o =>o.AppUserId == userId, includeProperties:"OrderDetails");
-                      
-            return View(orderHeaders);
+            IEnumerable<CertificationSlot> slots = _unitOfWork.CertificationSlot.GetAll();
+            BookingVM bookingVM = new BookingVM();
+            bookingVM.Slots = slots;
+
+            return View(bookingVM);
         }
 
         public IActionResult Book()
         {
-            var userId = HttpContext.User.GetUserId();
+            //var userId = HttpContext.User.GetUserId();
 
-            BookingVM = new BookingVM()
-            {
-                BookingList = _unitOfWork.Booking.GetAll(includeProperties: "Product")
-            };
+            //BookingVM = new BookingVM()
+            //{
+            //    BookingList = _unitOfWork.Booking.GetAll(includeProperties: "Product")
+            //};
 
             //BookingVM.OrderHeader.Name = _unitOfWork.AppUser.GetName(userId);
             //BookingVM.OrderHeader.Email = _unitOfWork.AppUser.GetEmail(userId);
@@ -55,11 +56,11 @@ namespace SCSWeb.Areas.Customer
             return View(BookingVM);
         }
 
-        [HttpPost]
-        [ActionName("Summary")]
-        public IActionResult SummaryPOST()
-        {
-            var userId = HttpContext.User.GetUserId();
+        //[HttpPost]
+        //[ActionName("Summary")]
+        //public IActionResult SummaryPOST()
+        //{
+        //    var userId = HttpContext.User.GetUserId();
 
             //BookingVM.BookingList = _unitOfWork.Booking.GetAll(u => u.AppUserId == userId,
             //    includeProperties: "Product");
@@ -79,10 +80,10 @@ namespace SCSWeb.Areas.Customer
             //BookingVM.OrderHeader.OrderStatus = SD.StatusPending;
 
             //_unitOfWork.OrderHeader.Add(BookingVM.OrderHeader);
-            _unitOfWork.Save(); // ???? 
+            //_unitOfWork.Save(); // ???? 
 
-            foreach (var Booking in BookingVM.BookingList)
-            {
+            //foreach (var Booking in BookingVM.BookingList)
+            //{
                 //var orderDetail = new OrderDetails()
                 //{
                 //    ProductId = Booking.ProductId,
@@ -91,17 +92,17 @@ namespace SCSWeb.Areas.Customer
                 //    Count = Booking.ProdCount
                 //};
                 //_unitOfWork.OrderDetails.Add(orderDetail);
-                _unitOfWork.Save(); // ??? 
+               // _unitOfWork.Save(); // ??? 
             }
 
-            _unitOfWork.Save();
+            //_unitOfWork.Save();
 
-            return RedirectToAction(nameof(OrderConfirmation), new { id = BookingVM.OrderHeader.Id });
-        }
+        //    return RedirectToAction(nameof(OrderConfirmation), new { id = BookingVM.OrderHeader.Id });
+        //}
 
-        public IActionResult OrderConfirmation(int id)
-        {
-            //OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "AppUser");
+        //public IActionResult OrderConfirmation(int id)
+        //{
+        //    //OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "AppUser");
             ////OrderDetails orderDetails = _unitOfWork.OrderDetails.Get(u => u.OrderHeaderId == id, includeProperties: "OrderHeader,Product");
 
             //_emailSender.SendEmailAsync(orderHeader.AppUser.Email, "New Order - SCS AB",
@@ -122,17 +123,17 @@ namespace SCSWeb.Areas.Customer
             //_unitOfWork.Booking.RemoveRange(Bookings);
             //_unitOfWork.Save();
 
-            return View(id);
-        }
+        //    return View(id);
+        //}
 
-        public IActionResult Remove(int BookingId)
-        {
-            //var BookingFromDb = _unitOfWork.Booking.Get(u => u.Id == BookingId, tracked: true);
-            //HttpContext.Session.SetInt32(SD.SessionBooking, _unitOfWork.Booking
-            //  .GetAll(u => u.AppUserId == BookingFromDb.AppUserId).Count() - 1);
-            //_unitOfWork.Booking.Remove(BookingFromDb);
-            //_unitOfWork.Save();
-            return RedirectToAction(nameof(Index));
-        }
+        //public IActionResult Remove(int BookingId)
+        //{
+        //    //var BookingFromDb = _unitOfWork.Booking.Get(u => u.Id == BookingId, tracked: true);
+        //    //HttpContext.Session.SetInt32(SD.SessionBooking, _unitOfWork.Booking
+        //    //  .GetAll(u => u.AppUserId == BookingFromDb.AppUserId).Count() - 1);
+        //    //_unitOfWork.Booking.Remove(BookingFromDb);
+        //    //_unitOfWork.Save();
+        //    return RedirectToAction(nameof(Index));
+        //}
     }
-}
+
