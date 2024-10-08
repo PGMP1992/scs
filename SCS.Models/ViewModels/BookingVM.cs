374b78748684ee691b387465e8b2787740ea8f7e
Pedro <pedro@dkltd.net>
2024-10-08 09:58:48 +0200

    Validating Voucher

--- SCS.Models/ViewModels/BookingVM.cs
+++ SCS.Models/ViewModels/BookingVM.cs
@@ -2,7 +2,7 @@
 {
     public class BookingVM
     {
-        //public int Id { get; set; }
+        public string VoucherId { get; set; }
         public IEnumerable<CertificationSlot> Slots { get; set; }
         //public OrderHeader OrderHeader { get; set; }
     }
