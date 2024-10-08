201e94b8463fb3c588bfa49f47f813c883abf13c
Pedro <pedro@dkltd.net>
2024-10-08 11:45:12 +0200

    Silly Annotation

--- SCS.Models/CertificationSlot.cs
+++ SCS.Models/CertificationSlot.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace SCS.Models;
 
@@ -18,6 +19,7 @@ public class CertificationSlot
     public DayOfWeek? DayOfWeek { get; set; }
 
     public List<DateOnly>? Dates { get; set; }
+    
     [ValidateNever]
     public IEnumerable<CertificationDay> CertificationDays { get; set; }
 }
