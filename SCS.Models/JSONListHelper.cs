bb490290f5070c5188264230894ce1375e65b2aa
Pedro <pedro@dkltd.net>
2024-10-18 15:01:47 +0200

    Tidynig up

--- SCS.Models/JSONListHelper.cs
+++ SCS.Models/JSONListHelper.cs
@@ -20,42 +20,15 @@ namespace SCS.Utility
             }
             return System.Text.Json.JsonSerializer.Serialize(eventList);
         }
-
-        //public static string GetResourceListJSONString(List<Models.Location> locations)
-        //{
-        //    var resourcelist = new List<Resource>();
-
-        //    foreach (var loc in locations)
-        //    {
-        //        var resource = new Resource()
-        //        {
-        //            id = loc.Id,
-        //            title = loc.Name
-        //        };
-        //        resourcelist.Add(resource);
-        //    }
-        //    return System.Text.Json.JsonSerializer.Serialize(resourcelist);
-        //}
     }
 
     public class Event
     {
-        //public int id { get; set; }
-        //public string title { get; set; }
-        //public DateTime start { get; set; }
-        //public DateTime end { get; set; }
-        //public int resourceId { get; set; }
-        //public string description { get; set; }
-        public int idd { get; set; }
+        public int id { get; set; }
         public string title { get; set; }    
         public DateOnly start { get; set; }
         public DateOnly end {  get; set; } 
     }
 
-    //public class Resource
-    //{
-    //    public int id { get; set; }
-    //    public string title { get; set; }
-    //}
 }
 
