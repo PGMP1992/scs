d21496d50c68653241f20995aba0bb2b3a3e1609
Pedro Martinez <pedro@dkltd.net>
2024-10-23 14:07:01 +0200

    Created Controller AppSettings

---
+++ SCS.Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCS.Models
+{
+    public class AppSettings
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Key1 { get; set; }
+        public string Key2 {  get; set; }
+    }
+}
