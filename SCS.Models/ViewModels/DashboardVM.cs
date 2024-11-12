7bd3692128e34ed6daeccaf020dd5bb07caebfc5
Pedro Martinez <pedro@dkltd.net>
2024-11-12 12:03:27 +0100

    Creating Dashboard

---
+++ SCS.Models/ViewModels/DashboardVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCS.Models.ViewModels
+{
+    public class DashboardVM
+    {
+        public IEnumerable<Product> Products {  get; set; }
+        public IEnumerable<OrderDetails> Orders { get; set; }
+        public IEnumerable<AppUser> Users { get; set; }
+        public IEnumerable<Booking> Bookings { get; set; }
+    }
+}
---
+++ SCSWeb/Areas/Admin/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SCS.Models;
+using SCS.Models.ViewModels;
+using SCS.Repository.IRepository;
+using SCS.Utility;
+
+namespace SCSWeb.Areas.Admin
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Dashboard
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<AppUser> users = await _unitOfWork.AppUser.GetAllAsync();
+            IEnumerable<Product> products = await _unitOfWork.Product.GetAllAsync(includeProperties: "Category");
+            IEnumerable<OrderDetails> orders = await _unitOfWork.OrderDetails.GetAllAsync();
+            IEnumerable<Booking> bookings = await _unitOfWork.Booking.GetAllAsync();
+
+            DashboardVM dashboardVM = new DashboardVM()
+            {
+                Users = users,
+                Products = products,
+                Orders = orders,
+                Bookings = bookings
+            };
+            return View(dashboardVM);
+        }
+
+        // GET: Dashboard/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
